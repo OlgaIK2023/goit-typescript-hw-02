db9e35249cfc2c0039ff5f88ff4c440cd5dbeaf9
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-05-06 17:26:45 +0300

    Update photos-api.ts

--- src/photos-api.ts
+++ src/photos-api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 let perPage = 15;
 
-export async function fetchPhotosByInput(inputSearch, page) {
+export async function fetchPhotosByInput <T>(inputSearch: string, page: number): Promise<T> {
     const ACCESS = 'W30QFQ5psdqdWjrQsgwvjelTQ8hIFC9FGVdtp8H3fvQ';
     const url = `https://api.unsplash.com/search/photos?query=${inputSearch}&client_id=${ACCESS}`;
-    const respons = await axios.get(url, {
+    const respons = await axios.get<T>(url, {
         params: {
           per_page: perPage,
           page: page}});

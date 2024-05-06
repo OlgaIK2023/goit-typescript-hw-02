58bfbc81a23ea87592cf3af4849288cdb5d4872c
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-05-06 17:02:59 +0300

    Initial commit

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,14 @@
+import css from "./LoadMoreBtn.module.css"
+
+
+const LoadMoreBtn = ({ onClickButton}) => {
+    return (
+        <>
+            <button type="button" id="load_btn"  className={css.load_more_btn} onClick={onClickButton}>Load more</button>
+            
+        </>
+        
+  )
+}
+
+export default LoadMoreBtn
